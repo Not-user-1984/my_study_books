c29ed35c172f9e7e590bbbc65f4dbfde17831c6a
Not-user-1984 <pluzhnikov.d@team.nekidaem.ru>
2023-11-15 09:01:44 +0300

    [Python к вершинам мастерства]  - add class vector2b

---
+++ Python к вершинам мастерсва/11.Обьекты_в_стиле_python/класс_vector/vector2b.py
@@ -0,0 +1,38 @@
+from array import array
+
+import math
+
+class Vector2b:
+    __match_args__ = ('x', 'y')
+    typecode = 'd'
+
+    def __int__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}(!r), {!r}'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+                bytes(array(self.typecode, self)))
+
+    def __eq__(self, other):
+        return
+
